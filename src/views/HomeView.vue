925eee29433514e7aa2dae03b5f85f7233a686de
thanawat-bcr <thanawat.bcr@gmail.com>
2025-03-14 16:36:07 +0700

    refactor: clean up initialized project example

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+<script setup lang='ts'>
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home-view">
+    <h1>Home View</h1>
+    <p>This is a home page</p>
+  </div>
 </template>
